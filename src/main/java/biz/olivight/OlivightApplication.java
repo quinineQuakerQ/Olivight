c46479630954bef423b4632c7f78352bbba46622
cinnamon <droplet.drizzler@yahoo.com>
2022-09-23 23:43:19 +0900

    quinine update 18

--- src/main/java/biz/olivight/OlivightApplication.java
+++ src/main/java/biz/olivight/OlivightApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
 @SpringBootApplication
 public class OlivightApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OlivightApplication.class, args);
 	}
-
 }
